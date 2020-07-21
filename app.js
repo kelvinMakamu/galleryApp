8518beeb11ee668c6fce2ba5ee945deffb2b2fea
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2020-07-21 10:08:55 +0300

    Heroku Deployment File

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(express.json());
 app.use('/', indexRouter);
 app.use('/image', imageRouter);
 //Server PORT
-const PORT = 8150;
+const PORT = process.env.PORT || 5000;
 //Start Server
 app.listen(PORT, function(){
 	console.log(`Server is listening on port ${PORT}`);
